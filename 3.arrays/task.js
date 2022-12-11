db9a54bc036f9aa37124370647c1124733b84639
alexc <alexchb.makarenko@yandex.ru>
2022-12-11 14:38:09 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1
+    .every((item, idx) => item === arr2[idx]); 
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+    .filter(user => user.gender === gender)
+    .map(student => student.age)
+    .reduce((summ, userAge, index, array) => {
+        if(index === array.length - 1) {
+            return summ / array.length;
+        }
+        return summ + userAge;
+    }, 0);
 }
